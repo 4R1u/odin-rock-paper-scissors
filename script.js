a9904eaa3202038b51d86531c56da4ca081d78c2
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-09-14 19:27:16 +0400

    Implement getComputerChoice()

---
+++ script.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;
+        default:
+            return "error";
+            break;
+    }
+}
