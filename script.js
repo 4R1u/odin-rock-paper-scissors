c142660a462dd3d53c5f59aaa8893ff557872df0
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-09-15 19:01:52 +0400

    Complete Project

--- script.js
+++ script.js
@@ -19,27 +19,41 @@ function getHumanChoice() {
     return prompt("Enter rock, paper, or scissors");
 }
 
-let humanScore = 0;
-let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    if (humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors") {
-        console.log("Invalid option!");
-        return;
-    }
-    if ((humanChoice === "rock" && computerChoice === "scissors") ||
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+        if (humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors") {
+            console.log("Invalid option!");
+            return;
+        }
+        if ((humanChoice === "rock" && computerChoice === "scissors") ||
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper"))
         {
             console.log("You win!");
             ++humanScore;
+            }
+        else if (humanChoice === computerChoice) {
+            console.log("Tie!");
         }
-    else if (humanChoice === computerChoice) {
-        console.log("Tie!");
+        else {
+            console.log("You lose!");
+            ++computerScore;
+        }
     }
-    else {
-        console.log("You lose!");
-        ++computerScore;
+    
+    for (let i = 0; i < 5; ++i) {
+        playRound(getHumanChoice(), getComputerChoice());
     }
+    if (humanScore > computerScore)
+        console.log("You win " + humanScore + ":" + computerScore + "!");
+    else if (humanScore < computerScore)
+        console.log("You lose " + humanScore + ":" + computerScore + "!");
+    else
+    console.log("You tied with the computer " + humanScore + ":" + computerScore + "!");
 }
+
+playGame();
